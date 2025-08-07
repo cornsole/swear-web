16c0fc2d6b9a9f252fa8209773010ad10fcccf02
console <kyhet0739@naver.com>
2025-08-07 12:07:50 +0900

    웹페이지 제작,프론트엔드 작업

--- server.go
+++ server.go
@@ -1,19 +1,38 @@
 package main
 
 import (
+	"errors"
+	"log/slog"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
-	//e = echo의 새 인스턴스입니다
+	// Echo instance
 	e := echo.New()
 
-	// "/"
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
+	// Middleware
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	// Routes
+	e.GET("/", index)
+	e.POST("/api/swear-filter", func(c echo.Context) error {
+		// 요청 처리 로직
+		return c.JSON(http.StatusOK, map[string]string{
+			"filtered_text": "사랑해요",
+		})
 	})
-	e.IPExtractor = echo.ExtractIPDirect()
-	e.Logger.Fatal(e.Start(":8080"))
+
+	// Start server
+	if err := e.Start(":8080"); err != nil && !errors.Is(err, http.ErrServerClosed) {
+		slog.Error("failed to start server", "error", err)
+	}
+}
+
+// Handler
+func index(c echo.Context) error {
+	return c.File("index.html")
 }
